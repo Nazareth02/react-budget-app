38c27e7b8c48850b2f2db3766b03c3f4239d86e3
Nazareth <nazares2002@gmail.com>
2023-03-05 18:45:18 +0300

    feat(p-4): add validation (without message)

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 import { FormEvent, useState } from "react";
-import { FieldValues, SubmitHandler, useForm, UseFormRegister } from "react-hook-form";
+import {
+  Controller,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormRegister,
+} from "react-hook-form";
 import { v4 } from "uuid";
 import { useExpensesContext } from "../../context/ExpensesContext/ExpensesContext";
 import { Expense } from "../../context/ExpensesContext/types";
@@ -15,6 +21,7 @@ export type FormValues = {
 
 export const Form = () => {
   const {
+    control,
     register,
     handleSubmit,
     reset,
@@ -34,8 +41,45 @@ export const Form = () => {
   return (
     <StyledForm onSubmit={handleSubmit(onSubmit)}>
       <InputGroup>
-        <Input name="name" placeholder="enter name ..." register={register} />
-        <Input name="cost" placeholder="enter cost ..." register={register} />
+        <Controller
+          name="name"
+          control={control}
+          render={({ field: { ref, ...rest } }) => (
+            <Input
+              type="text"
+              register={register}
+              placeholder="enter name ..."
+              {...rest}
+              {...register("name", {
+                required: "field 'name' is required!!!",
+                maxLength: {
+                  value: 15,
+                  message: "max length: 15 symbols",
+                },
+              })}
+            />
+          )}
+        />
+        <Controller
+          name="cost"
+          control={control}
+          render={({ field: { ref, ...rest } }) => (
+            <Input
+              type="number"
+              register={register}
+              placeholder="enter cost ..."
+              {...register("cost", {
+                required: "field 'cost' is required!!!",
+                maxLength: {
+                  value: 5,
+                  message: "max length: 5 symbols",
+                },
+              })}
+            />
+          )}
+          {...(errors.cost?.message && <span>{errors.cost?.message}</span>)}
+          {...(errors.name?.message && <span>{errors.name.message}</span>)}
+        />
       </InputGroup>
       <Button type="submit" />
     </StyledForm>
